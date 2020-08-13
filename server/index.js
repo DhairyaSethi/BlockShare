f31505dd7783e5657c4e850fdbae09bf8a538827
Dhairya Sethi <dhairya.rkp@gmail.com>
2020-08-13 20:08:50 +0530

    see blob file send

--- server/index.js
+++ server/index.js
@@ -1,63 +1,103 @@
 const port = 8000;
 const websocketServer = require('websocket').server;
 const http = require('http');
-const { connect } = require('http2');
+const { send } = require('process');
 const server = http.createServer();
 server.listen(port);
 const ws = new websocketServer({ httpServer: server});
+
 const clients = {}
 const users = []
 const userActivity = []
+let peerIds = []
+
+const types = {
+    NEW_PEER_FOUND: 'newpeer',
+    WELCOME: 'welcome',
+    RECIEVE_FILE: 'filerecieve',
+    SENT_FILE: 'filesend',
+    USER_ACTIVITY_LOG: 'log',
+
+}
+
+updatePeers = (id, type) => {
+    Object.kets(client).map(client => {
+        if(client !== id){
+            clients[client].sendUTF(JSON.stringify({
+                type: type,
+                peerId: client,
+                userActivity: userActivity
+            }))
+        }
+    })
+}
 
 ws.on('request', req => {
-    console.log('New Request. Key->', req.key)
     let connection = req.accept(null, req.origin)
-    const id = Math.floor(Math.random() * 100)
+    const id = 'peer' + Math.floor(Math.random() * 100).toString()
+    console.log('New Peer ==>', id, 'connected.')
+    userActivity.push( `${id} is in the house!`)
     clients[id] = connection;
+    peerIds.push(id)
+    
     connection.sendUTF(JSON.stringify({
-        type: 'welcome',
-        msg: 'hello'
+        type: types.WELCOME,
+        id: id
     }))
+
+    updatePeers(id, types.NEW_PEER_FOUND)
+
     connection.on('message', msg => {
+
         if(msg.type === 'utf8') {
-            const rec = JSON.parse(msg.utf8Data);
-            console.log('message from client ', rec)
-            switch(rec.type) {
-                case 'newuser': newUser(rec, connection, id)
-                case 'file' : fileRecieved(rec, connection)
+            const msgFomClient = JSON.parse(msg.utf8Data);
+
+            console.log(`Message from ${id} ==> ${msgFomClient}`)
+
+            updatePeers(id)
+
+            switch(msgFomClient.type) {
+                case types.SENT_FILE : 
+                    fileRecieved(msgFomClient, id)
+                    userActivity.push( `${id} just shared a document!`)
+                    updatePeers(id, types.USER_ACTIVITY_LOG)
                     
             }
         }
+        // if file sending as string isn't possible, then use this?
         if (msg.type === 'binary') {
-            console.log('File recieved ', msg, 'now sending file back')
+            console.log('File recieved ', msg, 'now sending file back')//this is a test, sending file back to sender
             connection.sendUTF(JSON.stringify({
-                type: 'filerec',
+                type: types.RECIEVE_FILE,
                 file: msg
             }))
         }
-        else {
-            console.log( ' file :( ', msg)
-        }
+    })
+
+    connection.on('close', connection => {
+        console.log('Peer ==>', id, 'disconneting.')
+        delete clients[id]
+        userActivity.push(`${id} has left.`)
+        updatePeers(id, types.USER_ACTIVITY_LOG)
+
     })
     
 })
 
-newUser = (data, conn, id) => {
-    users.push({
-        name: data.name,
-        id: id,
-        conn: conn
-    })
-    conn.sendUTF(JSON.stringify({
-        type: 'usersdetected',
-        name: data.name,
-        id: id
-    }))
-}
 
-const fileRecieved = (data, conn) => {
-    conn.sendUTF(JSON.stringify({
-        file: data.file,
-        type: 'sending file back'
-    }))
+const fileRecieved = (data, senderPeerId) => {
+    // sending file to all peers except senderpeer
+    //TODO add breaking file to chunks methods here
+    Object.keys(clients).map(peerId => {
+        if(peerId !== senderPeerId){
+            clients[peerId].sendUTF(
+                JSON.stringify({
+                type: types.RECIEVE_FILE,
+                file: data,
+                sentby: senderPeerId
+                })
+            )
+        }
+    })
+
 }
