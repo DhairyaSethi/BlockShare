1f4ed1620df6341aabaf95f0de966d17ab1d1284
Dhairya Sethi <dhairya.rkp@gmail.com>
2020-08-16 18:51:46 +0530

    more

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -4,18 +4,107 @@ import '../App.css';
 import Fileupload from './fileupload';
 import QRCode from 'qrcode.react';
 import UserActivity from './UserActivity'
+import {w3cwebsocket} from 'websocket';
 import Peer from 'peerjs'
-    
+
+let peer, conn;
+let clients = {};
+const ws = new w3cwebsocket('ws://127.0.0.1:8000')
+const types = {
+    NEW_PEER_FOUND: 'newpeer',
+    WELCOME: 'welcome',
+    RECIEVE_FILE: 'filerecieve',
+    SENT_FILE: 'filesend',
+    USER_ACTIVITY_LOG: 'log',
+    NEW_USER: 'newUser'
+
+}
+
 function Main (props) { 
     const [username, setUsername] = useState()
+    const [sendModal, setSendModal] = useState(false)
     const [loggedIn, setLoggedIn] = useState(false)
     const [userActivity, setUserActivity] = useState([])
+    const [peerId, setPeerId] = useState()
+    const [otherId, setOtherId] = useState()
+    const [file, setFile] = useState()
 
     const handleClick = () => {
         props.history.push("/dashboard");
     }
  
-    
+    // useEffect(() => {
+    //     if(!username) return
+    //         ws.send(JSON.stringify({
+    //             type: types.NEW_USER,
+    //             id: username
+    //         }))
+    // }, [username])
+
+    useEffect(() => {
+        ws.onmessage = _msg => {
+            let msg = JSON.parse(_msg.data)
+            console.log('message recieved =>', msg)
+      
+            switch(msg.type) {
+
+              case types.NEW_PEER_FOUND:
+                setOtherId(msg.peerId)
+                console.log('Other Peer found ', otherId)
+                break;
+      
+              case types.USER_ACTIVITY_LOG:
+                // setUserActivity(userActivity => [...userActivity, msg.userActivity])
+                setUserActivity(msg.userActivity)
+                break;
+            }
+        }
+    }, [])
+
+    useEffect(() => {
+        if(peerId) openPeer()
+
+    }, [peerId])
+
+    const openPeer =  () => {
+        peer = new Peer(peerId
+        ,{
+          host: 'localhost',
+          port: 9000,
+          path: '/',
+          debug: 3
+        }
+        )
+    }
+
+
+    const onUpload = () => {
+        if(!file) return
+        if(!file.type) {
+            alert('File type not recognized! Please try again.')
+            return
+        }
+        // const blob = new File([file], { type: file.type })
+        // const url = URL.createObjectURL(blob)
+        conn = peer.connect('rec')
+        conn.on('open', () => {
+            console.log(`[+] Sending ${file}`)
+            conn.send({file: file, type: file.type, name: file.name})
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     const Login = () => (
         <div className="Logincont">
         <div className="Loginin">
@@ -51,20 +140,22 @@ return (
     <div className="auth-inner">
     <form>
         <h3>Send</h3>
-
     </form> 
+    {!sendModal ? 
+        <button type="submit" className="btn btn-primary btn-block" onClick={() => {setPeerId('sen'); setSendModal(true);}}> Send </button>
+        :
+        <div>
+        <input type='file' onChange={e=> setFile(e.target.files[0])} />
+        {file ? <button onClick={onUpload}> Share! </button> : ''}
+        </div>
+    }
     <Fileupload />
-    <button type="submit" className="btn btn-primary btn-block"
-    onClick={handleClick}
-    >Send</button>
     </div>
     <div className="auth-inn">
     <form>
         <h3>Receive</h3>
     </form> 
-        <button type="submit" className="btn btn-primary btn-block"
-        onClick={handleClick}
-        >Receive</button>
+        <button type="submit" className="btn btn-primary btn-block" onClick={() => setPeerId('rec')}> Receive </button>
     </div>
     <div className="qrcode">
     <form>
