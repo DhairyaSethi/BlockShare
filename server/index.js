2ed6b51fac55723f56ca5731525f5f46ff170525
Dhairya Sethi <dhairya.rkp@gmail.com>
2020-08-16 20:36:31 +0530

    signalling server done

--- server/index.js
+++ server/index.js
@@ -19,75 +19,51 @@ peerServer.on('close', conn => {
     delete peers[conn.id]
 })
 
-ws.on('request', req => {
-    let connection = req.accept(null, req.origin)
-    const id = 'peer' + Math.floor(Math.random() * 100).toString()
+function NewUser(msgFomClient, connection) {
+    const {id} = msgFomClient
     console.log('New Peer ==>', id, 'connected.')
-    userActivity.push( `${id} is in the house!`)
+    userActivity.push(`${new Date().toLocaleTimeString('en-US')} : ${id} has just joined!`)
     clients[id] = connection;
     peerIds.push(id)
     
-    connection.sendUTF(JSON.stringify({
-        type: types.WELCOME,
-        id: id
-    }))
+}
 
-    updatePeers(id, types.NEW_PEER_FOUND)
+ws.on('request', req => {
+    let connection = req.accept(null, req.origin)
+    let peerId = ''
 
     connection.on('message', msg => {
 
         if(msg.type === 'utf8') {
             const msgFomClient = JSON.parse(msg.utf8Data);
+            console.log(`Message from ${connection.id} ==> ${msgFomClient}`)
 
-            console.log(`Message from ${id} ==> ${msgFomClient}`)
-
-            updatePeers(id)
 
             switch(msgFomClient.type) {
-                case types.SENT_FILE : 
-                    fileRecieved(msgFomClient, id)
-                    userActivity.push( `${id} just shared a document!`)
-                    updatePeers(id, types.USER_ACTIVITY_LOG)
-                    
+
+                case types.NEW_USER :
+                    NewUser(msgFomClient, connection)
+                    peerId = msgFomClient.id
+                    break;
+
+                case types.SENT_FILE :
+                    userActivity.push( `${new Date().toLocaleTimeString('en-US')} : ${peerId} just shared a document!`)
+                    updatePeers()
             }
-        }
-        // if file sending as string isn't possible, then use this?
-        if (msg.type === 'binary') {
-            console.log('File recieved ', msg, 'now sending file back')//this is a test, sending file back to sender
-            connection.sendUTF(JSON.stringify({
-                type: types.RECIEVE_FILE,
-                file: msg
-            }))
         }
     })
 
-    connection.on('close', connection => {
-        console.log('Peer ==>', id, 'disconneting.')
-        delete clients[id]
-        userActivity.push(`${id} has left.`)
-        updatePeers(id, types.USER_ACTIVITY_LOG)
+    connection.on('close', e => {
+        let peerId = Object.keys(clients).find(key => clients[key] === connection)
+        console.log('Peer ==>', peerId, 'disconneting.')
+        delete clients[peerId]
+        userActivity.push(`${new Date().toLocaleTimeString('en-US')} : ${peerId} has left.`)
+        updatePeers()
 
     })
     
 })
 
-const fileRecieved = (data, senderPeerId) => {
-    // sending file to all peers except senderpeer
-    //TODO add breaking file to chunks methods here
-    Object.keys(clients).map(peerId => {
-        //@FOR NOW SEND TOO EVERYONE, FIX THIS
-//        if(peerId !== senderPeerId) { 
-            clients[peerId].sendUTF(
-                JSON.stringify({
-                type: types.RECIEVE_FILE,
-                file: data,
-                sentby: senderPeerId
-                })
-            )
-//        }
-    })
-
-}
 
 const clients = {}
 const users = []
@@ -100,30 +76,15 @@ const types = {
     RECIEVE_FILE: 'filerecieve',
     SENT_FILE: 'filesend',
     USER_ACTIVITY_LOG: 'log',
+    NEW_USER: 'newUser'
 
 }
-
-
-
-updatePeers = (id, type) => {
-
-    if(type = types.NEW_PEER_FOUND) {
-        Object.keys(clients).map(client => {
-            let peer = client === peerIds[0] ? peerIds[1] : peerIds[0]
-            clients[client].sendUTF(JSON.stringify({
-                type: type,
-                peerId: peer
-            }))
-        })
-    }
-
+updatePeers = () => {
     Object.keys(clients).map(client => {
-        if(client !== id){
-            clients[client].sendUTF(JSON.stringify({
-                type: type,
-                peerId: client,
-                userActivity: userActivity
-            }))
-        }
+        clients[client].sendUTF(JSON.stringify({
+            type: types.USER_ACTIVITY_LOG,
+            peerId: client,
+            userActivity: userActivity
+        }))
     })
 }
